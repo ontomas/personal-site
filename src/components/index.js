9cbf9198e0c3a8d636893400717c9c973249a297
Tomas Oniščiukas <onisciukas.tomas@gmail.com>
2020-11-20 08:34:20 +0200

    Fix netflify build errors

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Header from "./Header"
 import Footer from "./Footer"
-import Layout from "./Layout"
-import SEO from "./SEO"
+import Layout from "./layout"
+import SEO from "./seo"
 import Animations from "./Animations"
 
 export { Header, Footer, Layout, SEO, Animations }
