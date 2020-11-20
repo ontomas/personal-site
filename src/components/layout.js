1d2a1cf9147db7beebaaa42c789c29c53197d0ae
Tomas Oniščiukas <onisciukas.tomas@gmail.com>
2020-11-20 08:15:33 +0200

    Fix netflify build errors

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import { Header, Footer } from "../components"
+import { Header, Footer } from "."
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
